7d0c7c0a571905963a14156afe614456a9d31fff
Richard <richard@discoursehosting.com>
2020-06-29 14:15:27 +0000

    Add to user_card as well

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,23 @@
 # name: extend-user
 # about: User Serializer Extender
-# version: 0.1
+# version: 0.2
 # authors: DiscourseHosting
 
 after_initialize do
-  add_to_serializer(:user, :external_username) { 
+  add_to_serializer(:user, :external_username) do
     unless object.single_sign_on_record.nil?
       object.single_sign_on_record.external_username
     else
-      "hello"
+      ""
     end
-  }
+  end
+
+  add_to_serializer(:user_card, :external_username) do
+    unless object.single_sign_on_record.nil?
+      object.single_sign_on_record.external_username
+    else
+      ""
+    end
+  end
+
 end
